6e81f1d546ccb716c6dbfa4e514a728ef2fb760a
haimianbao1111 <657003383@qq.com>
2020-02-14 17:17:49 +0800

    添加dockerfile文件

--- admin/obj/Release/netcoreapp3.1/admin.AssemblyInfo.cs
+++ admin/obj/Release/netcoreapp3.1/admin.AssemblyInfo.cs
@@ -11,9 +11,8 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("0ee5bb16-609c-45f5-9300-ab0ca2bce9ed")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("admin")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("admin")]
